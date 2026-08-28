4b5634b3ecd3e903610ba97efc2984c1255857b1
scaryhound <158007744+scaryhound@users.noreply.github.com>
2026-08-28 13:00:02 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-
 #Script to build buildroot configuration
 #Author: Siddhant Jajoo
-
-
-
 
 source shared.sh
 
@@ -15,27 +11,36 @@ git submodule update
 
 set -e 
 cd `dirname $0`
-# Hotfix for QEMU Python bug without touching the workflow
+
+# Hotfix for QEMU Python bug - Must be injected at the TOP of the mk file!
 if ! grep -q "HOST_QEMU_PATCH_MKVENV" buildroot/package/qemu/qemu.mk; then
-    printf "\ndefine HOST_QEMU_PATCH_MKVENV\n\tsed -i '/distlib.scripts.ScriptMaker/d' \$(@D)/python/scripts/mkvenv.py\n\tsed -i '/maker.make/d' \$(@D)/python/scripts/mkvenv.py\nendef\nHOST_QEMU_POST_EXTRACT_HOOKS += HOST_QEMU_PATCH_MKVENV\n" >> buildroot/package/qemu/qemu.mk
+    echo "Injecting QEMU patch at the top of qemu.mk..."
+    
+    # 1. Create a temporary file with our patch
+    printf "define HOST_QEMU_PATCH_MKVENV\n\tsed -i '/distlib.scripts.ScriptMaker/d' \$(@D)/python/scripts/mkvenv.py\n\tsed -i '/maker.make/d' \$(@D)/python/scripts/mkvenv.py\nendef\nHOST_QEMU_POST_EXTRACT_HOOKS += HOST_QEMU_PATCH_MKVENV\n\n" > qemu_patch_temp.mk
+    
+    # 2. Append the original QEMU mk file below our patch
+    cat buildroot/package/qemu/qemu.mk >> qemu_patch_temp.mk
+    
+    # 3. Overwrite the original file
+    mv qemu_patch_temp.mk buildroot/package/qemu/qemu.mk
 fi
 
 if [ ! -e buildroot/.config ]
 then
-	echo "MISSING BUILDROOT CONFIGURATION FILE"
+    echo "MISSING BUILDROOT CONFIGURATION FILE"
 
-	if [ -e ${AESD_MODIFIED_DEFCONFIG} ]
-	then
-		echo "USING ${AESD_MODIFIED_DEFCONFIG}"
-		make -C buildroot defconfig BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} BR2_DEFCONFIG=${AESD_MODIFIED_DEFCONFIG_REL_BUILDROOT}
-	else
-		echo "Run ./save_config.sh to save this as the default configuration in ${AESD_MODIFIED_DEFCONFIG}"
-		echo "Then add packages as needed to complete the installation, re-running ./save_config.sh as needed"
-		make -C buildroot defconfig BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} BR2_DEFCONFIG=${AESD_DEFAULT_DEFCONFIG}
-	fi
+    if [ -e ${AESD_MODIFIED_DEFCONFIG} ]
+    then
+        echo "USING ${AESD_MODIFIED_DEFCONFIG}"
+        make -C buildroot defconfig BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} BR2_DEFCONFIG=${AESD_MODIFIED_DEFCONFIG_REL_BUILDROOT}
+    else
+        echo "Run ./save_config.sh to save this as the default configuration in ${AESD_MODIFIED_DEFCONFIG}"
+        echo "Then add packages as needed to complete the installation, re-running ./save_config.sh as needed"
+        make -C buildroot defconfig BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} BR2_DEFCONFIG=${AESD_DEFAULT_DEFCONFIG}
+    fi
 else
-	echo "USING EXISTING BUILDROOT CONFIG"
-	echo "To force update, delete .config or make changes using make menuconfig and build again."
-	make -C buildroot BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT}
-
+    echo "USING EXISTING BUILDROOT CONFIG"
+    echo "To force update, delete .config or make changes using make menuconfig and build again."
+    make -C buildroot BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT}
 fi
