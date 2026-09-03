3d198dea7a884651d592f98d74027b43fabd4802
scaryhound <shaunmathias440@gmail.com>
2026-09-04 00:20:12 +0530

    Fix: Use absolute paths in external.mk for all packages

--- base_external/external.mk
+++ base_external/external.mk
@@ -1,1 +1,2 @@
-include ../base_external/package/aesd-assignments/aesd-assignments.mk
+include $(BR2_EXTERNAL_PROJECT_BASE_PATH)/package/aesd-assignments/aesd-assignments.mk
+include $(BR2_EXTERNAL_PROJECT_BASE_PATH)/package/ldd/ldd.mk
