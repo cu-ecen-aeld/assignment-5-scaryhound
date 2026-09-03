266724119982a665474d6973860d356b7ffb49f7
scaryhound <shaunmathias440@gmail.com>
2026-09-04 00:54:46 +0530

    Fix: Restore lowercase project_base paths and force make clean

--- base_external/external.mk
+++ base_external/external.mk
@@ -1,2 +1,2 @@
-include $(BR2_EXTERNAL_PROJECT_BASE_PATH)/package/aesd-assignments/aesd-assignments.mk
-include $(BR2_EXTERNAL_PROJECT_BASE_PATH)/package/ldd/ldd.mk
+include $(BR2_EXTERNAL_project_base_PATH)/package/aesd-assignments/aesd-assignments.mk
+include $(BR2_EXTERNAL_project_base_PATH)/package/ldd/ldd.mk
